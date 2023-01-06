3c730da253d9f30dbd59d48ba84c347a4d750035
Julius Djudjaku <juliusdjudjaku@gmail.com>
2023-01-06 10:14:30 +0100

    connect with mongoose

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/api-routes/
+import db from '../../utils/db'
+import User from '../../models/user';
+import Product from '../../models/Product';
+import mongoose from "mongoose";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+const prodIds = [new mongoose.Types.ObjectId()];
+
+export const productsArr = [
+  {
+    _id: prodIds[0],
+    sku: "00AQW127",
+    name: "Test product",
+    picturePath: "test.jpeg",
+    price: 999,
+    qty: 2,
+    category: 'Keyboards',
+  },
+];
+
+export default async function handler(req, res) {
+  await db.connect();
+  const products = await Product.insertMany(productsArr)
+  await db.disconnect();
+
+
+
+  res.status(200).json(products)
 }
